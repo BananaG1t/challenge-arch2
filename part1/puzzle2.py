de781ce77f7f63e97bce8e9a0174ccf00c250414
BananaG1t <bananamain@riseup.net>
2023-10-31 10:58:29 +0100

    puzzle2

--- part1/puzzle2.py
+++ part1/puzzle2.py
@@ -10,7 +10,19 @@ What is the registration number of Chris?
 from math import sqrt
 
 
-for i in range(1001):
-    if sqrt(i).is_integer():
-        if sqrt(i + 105).is_integer():
-            print(i + 105)
+primenumbs = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+              43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
+
+for number in range(10000001):
+    if sqrt(number).is_integer():
+        if sqrt(number + 105).is_integer():
+            times = 0
+            two = False
+            for prime in primenumbs:
+                if number % prime == 0:
+                    times += 1
+                if times == 2 and not two:
+                    print(number)
+                    two = True
+                if times > 2:
+                    print(f"{number} has too many")
